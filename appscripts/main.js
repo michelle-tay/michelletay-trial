b30f2e8344be4c1bebdd7488ac4bb3b1171813d4
michelle-tay <89303604+michelle-tay@users.noreply.github.com>
2023-09-03 02:51:30 +0800

    Ver. 1

---
+++ appscripts/main.js
@@ -0,0 +1,7 @@
+function jumptoabout(){
+    document.getElementById("aboutme").scrollIntoView();
+}
+
+function windowprompt(){ 
+    alert("Thank you for visting my portfolio! Do note this site is still having on-going changes! (ꈍᴗꈍ)");
+  }
